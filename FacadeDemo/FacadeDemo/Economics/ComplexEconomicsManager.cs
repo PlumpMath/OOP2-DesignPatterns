eac37fe7d7cc0f0e7ef2d76d9848543b8654530f
Viktor Gustafsson <viktor.b.gustafsson@gmail.com>
2016-09-26 11:07:46 +0200

    added faced design pattern example

--- FacadeDemo/FacadeDemo/Economics/ComplexEconomicsManager.cs
+++ FacadeDemo/FacadeDemo/Economics/ComplexEconomicsManager.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace FacadeDemo.Economics
 {
+    //reprecens complex economics ssytem
     class ComplexEconomicsManager
     {
+       
     }
 }
---
+++ FacadeDemo/FacadeDemo/EconomicsFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacadeDemo.Economics;
+
+namespace FacadeDemo
+{
+    static class EconomicsFacade
+    {
+        public static bool PaySalary(int empId) {
+            dynamic manager = new ComplexEconomicsManager();
+            var emp = manager.GetEmplyee(empId);
+            var salary = emp.GetMonthlySalary();
+            var receipt = manager.ProcessTransaction(emp, salary);
+            return receipt.IsValid();
+        }
+    }
+}
