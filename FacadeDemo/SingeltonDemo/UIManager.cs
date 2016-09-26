ebdec827ce1975c42b2a98bffb753a046998bd09
Viktor Gustafsson <viktor.b.gustafsson@gmail.com>
2016-09-26 12:02:46 +0200

    added things

---
+++ FacadeDemo/SingeltonDemo/UIManager.cs
@@ -0,0 +1,17 @@
+﻿namespace SingeltonDemo {
+    internal class UiManager {
+        private static UiManager instance;
+
+        private UiManager() {}
+
+        public static UiManager Instance {
+            get {
+                if (instance.Equals(null))
+                {
+                    instance = new UiManager();
+                }
+                return instance;
+            }
+        }
+    }
+}
